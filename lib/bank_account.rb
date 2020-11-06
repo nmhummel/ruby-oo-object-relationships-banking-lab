977e1c1198bdbbf2e29e0adcadfefc9b67708c48
nmhummel <natalie.hummel@gmail.com>
2020-11-05 22:55:23 -0600

    commit tree 56daa0fe2eb36e9c81364003da08d49c396767a4

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,44 @@
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name, balance=1000, status="open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
+
+    def deposit(amount)
+        @balance += balance
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @balance > 0 && @status=="open" ? true : false
+    end
+
+    def close_account
+        @status = "closed" 
+    end
+
 end
+
+# BankAccount  
+#   #initialize
+#     can initialize a Bank Account (FAILED - 1)
+#     initializes with a name (FAILED - 2)
+#     always initializes with balance of 1000 (FAILED - 3)
+#     always initializes with a status of 'open' (FAILED - 4)
+#     can't change its name (FAILED - 5)
+#   #deposit
+#     can deposit money into its account (FAILED - 6)
+#   #display_balance
+#     can display its balance (FAILED - 7)
+#   #valid?
+#     is valid with an open status and a balance greater than 0 (FAILED - 8)
+#   #close_account
+#     can close its account (FAILED - 9)
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,69 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid? ? true : false
+  end 
+
+  # def execute_transaction
+  #   if valid? && sender.balance > 0 && @status == "pending"
+  #     sender.balance -= amount
+  #     receiver.balance += amount
+  #     @status = "complete"
+  #   else 
+  #     @status = "rejected"
+  #     "Transaction rejected. Please check your account balance."
+  #   end
+  # end
+
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.status =  "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += @amount
+      receiver.balance -= @amount
+      self.status = "reversed"
+    end
+  end
+
+
 end
+
+
+
+# Transfer
+# initialize
+#   can initialize a Transfer (FAILED - 10)
+#   initializes with a sender (FAILED - 11)
+#   initializes with a receiver (FAILED - 12)
+#   always initializes with a status of 'pending' (FAILED - 13)
+#   initializes with a transfer amount (FAILED - 14)
+# #valid?
+#   can check that both accounts are valid (FAILED - 15)
+#   calls on the sender and receiver's #valid? methods (FAILED - 16)
+# #execute_transaction
+#   can execute a successful transaction between two accounts (FAILED - 17)
+#   each transfer can only happen once (FAILED - 18)
+#   rejects a transfer if the sender does not have enough funds (does not have a valid account) (FAILED - 19)
+# #reverse_transfer
+#   can reverse a transfer between two accounts (FAILED - 20)
+#   it can only reverse executed transfers (FAILED - 21)
+
+
